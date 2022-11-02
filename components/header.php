9521f20cd49fa9976550fe4691df3b5dbc1d2131
cirocki <marcincirocki89@gmail.com>
2022-11-02 21:57:34 +0100

    Add menu mobile styling

--- components/header.php
+++ components/header.php
@@ -8,24 +8,29 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 <!-- HEADER  -->
 <header class="main-header">
     <!-- <div class="container"> -->
-        <div class="menu-wrapper">
-            <div class="top-part">
-                <div class="branding-wrapper">
-                    <a class="logo" href="/">
-                        <span class="icon-logo"></span>
-                        <span class="icon-sygnet"></span>
-                    </a>
-                </div>
-                <div class="hamburger-wrapper">
-                    <div id="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation"
-                        class="hamburger hamburger--spin js-hamburger ">
-                        <div class="hamburger-box">
-                            <div class="hamburger-inner"></div>
+    <div class="menu-wrapper">
+        <div class="top-part">
+            <div class="container-in">
+                <div class="top-content-wrapper">
+
+                    <div class="branding-wrapper">
+                        <a class="logo" href="/">
+                            <span class="icon-logodark"></span>
+                        </a>
+                    </div>
+                    <div class="hamburger-wrapper">
+                        <div id="navbar-toggler" aria-expanded="false" aria-label="Toggle navigation" class="hamburger hamburger--spin js-hamburger ">
+                            <div class="hamburger-box">
+                                <div class="hamburger-inner"></div>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
-            <div class="bot-part">
+        </div>
+        <div class="bot-part">
+            <div class="container-in">
                 <nav class="nav-primary">
                     <div class="menu-primary-menu-container">
                         <ul id="menu-primary-menu" class="menu">
@@ -37,10 +42,8 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
                             </li>
                             <li class="menu-item <?= ($activePage == 'oferta') ? 'active' : ''; ?>"><a href="/o-mnie">O
                                     mnie</a></li>
-                            <li class="menu-item <?= ($activePage == 'kontakt') ? 'active' : ''; ?>"><a
-                                    href="/pytania-odpowiedzi">Q&A</a></li>
-                            <li class="menu-item <?= ($activePage == 'kontakt') ? 'active' : ''; ?>"><a
-                                    href="/kontakt">Kontakt</a></li>
+                            <li class="menu-item <?= ($activePage == 'kontakt') ? 'active' : ''; ?>"><a href="/pytania-odpowiedzi">Q&A</a></li>
+                            <li class="menu-item <?= ($activePage == 'kontakt') ? 'active' : ''; ?>"><a href="/kontakt">Kontakt</a></li>
                         </ul>
                         <ul id="menu-social-menu" class="menu">
                             <li class="menu-item "><a href="#instagram">IG</a></li>
@@ -50,6 +53,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
                 </nav>
             </div>
         </div>
+    </div>
     <!-- </div> -->
 </header>
 <!-- / HEADER  -->
